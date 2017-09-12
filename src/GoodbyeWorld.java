e5687d1f0f5b89cd0c6e8f81858200bad37ead36
nadstermonster <nadstermonster@172.16.73.3>
2017-09-12 11:30:53 -0600

    OKOKOKOKOKOK

--- src/GoodbyeWorld.java
+++ src/GoodbyeWorld.java
@@ -9,7 +9,7 @@ public class GoodbyeWorld {
 	public static void main(String args[]){
 		System.out.println("Goodbye World!!!! :)))");
 		System.out.println("AHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-		System.out.println("AHHHHHHHHHHHHHHHHHHHHHHHHHHHH OMGOMGOMG");
+		System.out.println("AHHHHHHHHHHHHHHHHHHHHHHHHHHHH OMGOMGOMG.OKOKOKOKOKOKOK");
 
 		
 		
